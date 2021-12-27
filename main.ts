9d5f5e6f4a988f123b9d02a00319ad34a89397f6
JuanLuisUS <61232280+JuanLuisUS@users.noreply.github.com>
2021-12-27 10:22:43 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    OLED12864_I2C.clear()
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    OLED12864_I2C.showNumber(
+    0,
+    0,
+    pins.analogReadPin(AnalogPin.P1),
+    5
+    )
+    OLED12864_I2C.showNumber(
+    0,
+    10,
+    input.lightLevel(),
+    1
+    )
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    OLED12864_I2C.clear()
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    OLED12864_I2C.showNumber(
+    0,
+    0,
+    input.temperature(),
+    3
+    )
+})
+OLED12864_I2C.init(60)
+music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+pins.digitalWritePin(DigitalPin.P8, 0)
+pins.digitalWritePin(DigitalPin.P12, 0)
+pins.digitalWritePin(DigitalPin.P16, 0)
